90fea24215bdd434808a2ce4e4e61700871f5eda
jwenz723 <jwenz723@gmail.com>
2019-09-11 23:59:19 -0600

    Added contributors endpoint to backend

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 const HELLO_RESPONSE = "Hello, from the underworld!\n"
@@ -14,6 +15,7 @@ func main() {
 	addr := flag.String("addr", ":8081", "address to run the backend server on")
 	flag.Parse()
 
+	http.Handle("/contributors", handleContributors())
 	http.Handle("/hello", handleHello())
 	fmt.Printf("starting backend http on %s\n", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
@@ -23,5 +25,13 @@ func handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		fmt.Printf("handled hello request\n")
 		io.WriteString(w, HELLO_RESPONSE)
+	}
+}
+
+func handleContributors() http.HandlerFunc {
+	contributors := []string{"Jeff Wenzbauer"}
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Printf("handled contributors request\n")
+		io.WriteString(w, strings.Join(contributors, "\n"))
 	}
 }
